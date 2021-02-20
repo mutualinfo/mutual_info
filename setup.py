7983379b47e49af1e3206466b5d62a1d0b6eae9e
David Cottrell <cottrell@users.noreply.github.com>
2021-02-20 22:38:52 +0000

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mutual_info",
-    version="0.3",
+    version="0.31",
     description="Putting GaelVaraquaux's gist in a repo. Mutual information calculation utils.",
     url="https://github.com/mutualinfo/mutual_info",
     long_description_content_type="text/markdown",
